5aa3f0662175eeab8c13ba1b8792e8d8c522c1b6
jayamaalathy-datascience <58150949+jayamaalathy-datascience@users.noreply.github.com>
2024-03-26 18:05:13 +0530

    Add files via upload

--- test2.py
+++ test2.py
@@ -7,55 +7,52 @@ import google.generativeai as genai
 from IPython.display import display
 from IPython.display import Markdown
 
+from pymongo import MongoClient
+from flask import Flask, request, render_template
 
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['mydatabase']
+collection = db['mocktest']
 # Used to securely store your API keycd
 #from google.colab import userdata
+
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-#GOOGLE_API_KEY="AIzaSyC0F96L7tARVV3XeeS5aVGYPF95eZ42WC8"
-#GOOGLE_API_KEY=
-# userdata.get('GOOGLE_API_KEY')
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-for modellist in genai.list_models():
-  if 'generateContent' in modellist.supported_generation_methods:
-    print(modellist.name)
 
 
 
 model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("I am looking for a mock paper for JEE, I don't want any explanation or. instruction in it just the whole question paper, it should be of only question no answer should be involved. I want all 90 question to be displayed.Show me all 90 question in mcq format All 1-90 question SO first show all question from 1-30 then 31-60 and in last 61-90.")
+response = model.generate_content("Generate a mock paper for JEE exam with 90 questions to be displayed under div section of html page, I don't want any explanation or instruction in it just the whole question paper, it should be of only question no answer should be involved")
 
 
-print(response.text)
+
+document = {'Questions': response.text}
+collection.insert_one(document)
+
+# Example: Query documents
+for doc in collection.find():
+    print("From mongodb",doc)
 
 
-from flask import Flask, render_template, request
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return render_template('index.html')
+    # Define some values to display
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Get data from the form
-    data_from_form = request.form['country']
-    data_from_form2 = request.form['exam']
-
-    # Do something with the data (your Python code)
-    # For example, print it to the console
-    print(f"Data from form: {data_from_form}{data_from_form2}")
-
-    # You can return a response if needed
-    return "Form submitted successfully!"
+    # Pass these values to the template
+    return render_template('mockPage.html', Questions=response.text)
 
 if __name__ == '__main__':
     app.run(debug=True)
